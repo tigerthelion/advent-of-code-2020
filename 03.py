d95830cded248f3313710cbfe46f3a47a0b4f96d
Brett <bjt.thompson@gmail.com>
2021-01-10 20:12:33 -0500

    unworking version

--- 03.py
+++ 03.py
@@ -5,26 +5,46 @@ https://adventofcode.com/2020/day/3
 """
 with open("inputs/03.txt") as f:
     # read in and parse data
-    map = []
+    map = ""
+
     data = f.readlines()
+    cols = len(data[0].split()[0])
+    rows = 0
     for line in data:
         lsplt = line.split()
-        map.append(lsplt[0])
 
-rows = len(map)
-col = 3
-colmax = len(map[0]) - 1
-rowi = 0
-seek = 0
-trees = 0
-while rowi < rows:
-    if map[rowi][seek] == '#':
-        trees += 1
+        map += lsplt[0]
+        rows += 1
+print(map)
+print(rows)
+print(cols)
 
-    if seek > colmax - col:
-        seek = seek - colmax - 1
+register = []
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+for i in slopes:
 
-    seek += col
+    # rows = len(map)
+    # col = 3
+    # colmax = len(map[0]) - 1
+    # rowi = 0
+    # seek = 0
+    # trees = 0
+    # while rowi < rows:
+    #     if map[rowi][seek] == '#':
+    #         trees += 1
 
-    rowi += 1
-print(trees)
+    #     if seek > colmax - col:
+    #         seek = seek - colmax - 1
+
+    #     seek += col
+
+    #     rowi += row_seek
+    # print(trees)
+
+    # [1,1], [1,2], [1,4], [1, 6], [2, 1]
+    # [2,2], [2,5], [2,9], [2, 13], [4, 2]
+
+
+def calc_trees(hill, slope: tuple):
+    rows = len(map)
+    col, row = slope
